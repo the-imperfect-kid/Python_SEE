54140da4fd17f696ed889c7b79af63bb5afdb3bb
the-imperfect-kid <76111886+the-imperfect-kid@users.noreply.github.com>
2020-12-17 12:49:33 +0530

    Update debug_exam.py

--- debug_exam.py
+++ debug_exam.py
@@ -50,9 +50,9 @@ def uniqueUpdate(data1, data2):
                 dupKeys[k] = [v1, v2]
                 # Remove (k, v1) from data1
                 del data1[k]
-            else:
-                # Add (k, v2) to data1
-                data1[k] = v2
+        else:
+            # Add (k, v2) to data1
+            data1[k] = v2
     # After processing all (k, v2) in
     # data2, return the dictionary
     return dupKeys
